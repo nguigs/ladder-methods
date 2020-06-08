9d3b3f08d1c6263f92a5bc6a21139821280be421
nguigs <nicolas.guigui@inria.fr>
2020-06-08 21:38:24 +0200

    add vizualisation of SL in notebook

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 
 import geomstats.backend as gs
+import geomstats.visualization as visualization
 from geomstats.geometry.hypersphere import Hypersphere
 from geomstats.geometry.spd_matrices import SPDMatrices, SPDMetricAffine,\
     Matrices
@@ -55,3 +56,43 @@ def make_plot(
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.legend(loc='best')
+
+
+def show_schild(tan_a, tan_b, point, n_rungs=4, n_points=15):
+    metric = SPHERE.metric
+    ladder = metric.ladder_parallel_transport(
+        tan_a, tan_b, point, n_rungs=n_rungs, return_geodesics=True,
+        scheme='schild')
+    pole_ladder = ladder['transported_tangent_vec']
+    trajectory = ladder['trajectory']
+    first_geo = metric.geodesic(initial_point=point, initial_tangent_vec=tan_a)
+
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    sphere_visu = visualization.Sphere(n_meridians=30)
+    ax = sphere_visu.set_ax(ax=ax)
+
+    t = gs.linspace(0, 1, n_points)
+    t_diag = gs.linspace(0, 1, n_points * 4)
+    sphere_visu.draw_points(ax, first_geo(t), marker='o', c='green', s=2)
+    for points in trajectory:
+        main_geodesic, diagonal, second_diagonal, final_geodesic = points
+        sphere_visu.draw_points(
+            ax, main_geodesic(t_diag), marker='o', c='blue', s=2)
+        sphere_visu.draw_points(ax, diagonal(t), marker='o', c='r', s=2)
+        sphere_visu.draw_points(ax, second_diagonal(t), marker='o', c='r', s=2)
+        sphere_visu.draw_points(
+            ax, final_geodesic(t), marker='o', c='green', s=2)
+
+    tangent_vectors = gs.stack(
+        [tan_b / n_rungs, tan_a, pole_ladder], axis=0)
+    base_point = gs.to_ndarray(point, to_ndim=2)
+    origin = gs.concatenate(
+        [base_point, base_point, final_geodesic(0.)], axis=0)
+    ax.quiver(
+        origin[:, 0], origin[:, 1], origin[:, 2],
+        tangent_vectors[:, 0], tangent_vectors[:, 1], tangent_vectors[:, 2],
+        color=['black', 'black', 'black'],
+        linewidth=2)
+    sphere_visu.draw(ax, linewidth=1)
+    return fig
